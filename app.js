0ce81f8444bef54d9e4adc96b026ba99f4d32262
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-09-07 13:16:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+// 1. Write a JavaScript function to calculate the sum of two numbers.  
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// let a = 10;
+// let b = 12;
+
+// console.log(sum(a, b));
+
+// 2. Write a JavaScript program to find the maximum number in an array. 
+
+// let a = [2, 13, 54, 34, 2];
+
+// function maxNumber(arr){
+//     return Math.max(...arr);
+// }
+
+// console.log(maxNumber(a));
+
+// 3. Factorial
+
+// function factorial(number) {
+
+//     if (number === 0 || number === 1) {
+
+//         return 1;
+
+//     } else {
+
+//         return number * factorial(number-1);
+
+//     }
+
+// }
+
+
+// console.log(factorial(5));
+
+
+// 4. Prime function
+// function isPrime(num) { 
+
+//     if (num <= 1) return false; 
+  
+//     for (let i = 2; i <= Math.sqrt(num); i++) { 
+  
+//       if (num % i === 0) return false; 
+  
+//     } 
+  
+//     return true; 
+  
+//   }
+  
+//   console.log(isPrime(5));
+
+// name = "ankit";
+
+// console.log(name);
+
+// console.log(typeof [])
+
+
