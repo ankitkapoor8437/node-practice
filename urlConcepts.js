fae2152791432266281c86e0a94059bc7592d854
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-10-18 15:41:09 +0530

    URL CONCEPTS!

---
+++ urlConcepts.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') return res.end();
+    const log = `${Date.now()}: ${req.url} Received\n`
+    const myUrl = url.parse(req.url, true);
+    
+    console.log(myUrl);
+
+    fs.appendFile('log.txt', log, (error, result) => {
+        switch (myUrl.pathname) {
+            case "/":
+                res.end("Changes done!");
+                break;
+            case "/about":
+                const username = myUrl.query.myname;
+                res.end(`Server about ${username}`);
+                break;
+            default:
+                res.end("404 Not found!");
+                break;
+        }
+    });
+    console.log();
+});
+
+const PORT = 9000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
