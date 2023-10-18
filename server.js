e782789bc1f2138c6348365a50adb2fedcdb639c
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-10-18 19:45:00 +0530

    CRUD on the mockup data!

--- server.js
+++ server.js
@@ -1,15 +1,55 @@
 const express = require("express");
+const usersData = require("./MOCK_DATA.json");
 
 // app is a handler function
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Created a homepage handler");
+// SSR
+app.get('/users', (req, res) => {
+    const html = ` <ul>
+            ${usersData.map((users) => `<li>${users.first_name}</li>`).join("")}
+        </ul>`
+    res.send(html);
 });
 
-app.get('/about',(req,res)=>{
-    res.send("Created a about handler");
+// CSR
+app.get('/api/users/', (req, res) => {
+    return res.json(usersData);
 });
+
+// Dynamic Path Parameters
+// app.get('/api/users/:id', (req, res) => {
+//     const id = Number(req.params.id);
+//     const user = usersData.find((users)=> users.id === id);
+//     console.log(user);
+//     return res.json(user);
+// });
+
+app.route("/api/users/:id").get((req, res) => {
+    const id = Number(req.params.id);
+    const user = usersData.find((users) => users.id === id);
+    console.log(user);
+    return res.json(user);
+})
+    .patch((req, res) => {
+        return res.json({ status: "pending" })
+    })
+    .delete((req, res) => {
+        return res.json({ status: "pending" })
+    });
+
+
+app.post('/api/users', (req, res) => {
+    res.json({ status: "pending" })
+});
+
+// app.patch('/api/users/:id', (req, res) => {
+//     res.json({ status: "pending" })
+// });
+
+// app.delete('/api/users/:id', (req, res) => {
+//     res.json({ status: "pending" })
+// });
 
 
 const PORT = 9000;
@@ -17,4 +57,3 @@ const PORT = 9000;
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 });
-  
