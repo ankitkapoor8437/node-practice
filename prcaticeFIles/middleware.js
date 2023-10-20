eb008f4b5e3e08acfa79448f6d249152011673b2
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-10-20 12:25:08 +0530

    CRUD IMPLIMENTATION USING MONGO DB!

---
+++ prcaticeFIles/middleware.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const usersData = require("./MOCK_DATA.json");
+const fs = require("fs");
+const { error } = require("console");
+
+// app is a handler function
+const app = express();
+const PORT = 9000;
+
+// middle ware example
+// app.use((req, res, next)=>{
+//     console.log("working");
+//     return res.json({message: "Working"})
+//     next();
+// })
+
+
+app.use(express.json())
+// SSR
+app.get('/users', (req, res) => {
+    const html = ` <ul>
+            ${usersData.map((users) => `<li>${users.first_name}</li>`).join("")}
+        </ul>`
+    res.send(html);
+});
+
+// CSR
+app.get('/api/users/', (req, res) => {
+    return res.json(usersData);
+});
+
+app.route("/api/users/:id").get((req, res) => {
+    const id = Number(req.params.id);
+    const user = usersData.find((users) => users.id === id);
+    console.log(user);
+    return res.json(user);
+})
+    .patch((req, res) => {
+        const id = Number(req.params.id);
+        const { first_name, last_name, email, gender, job_title } = req.body;
+        const dataToUpdate = usersData.find((users) => users.id === id);
+        console.log(dataToUpdate)
+        if (dataToUpdate) {
+            if (first_name) {
+                dataToUpdate.first_name =first_name;
+            }
+            
+            if (last_name) {
+                dataToUpdate.last_name =last_name;
+            }
+            
+            if (email) {
+                dataToUpdate.email =email;
+            }
+            
+            if (gender) {
+                dataToUpdate.gender =gender;
+            }
+            
+            if (job_title) {
+                dataToUpdate.job_title =job_title;
+            }
+        }
+          // Write the updated data to the JSON file
+          fs.writeFile('./MOCK_DATA.json', JSON.stringify(usersData), (error) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
+            return res.status(201).json(dataToUpdate);
+        });
+    })
+    .delete((req, res) => {
+        const id = Number(req.params.id);
+        const updatedUsersData = usersData.filter((user) => user.id !== id);
+
+        if (updatedUsersData.length < usersData.length) {
+            // Write the updated data to the JSON file
+            fs.writeFile('./MOCK_DATA.json', JSON.stringify(updatedUsersData), (error) => {
+                if (error) {
+                    console.error(error);
+                    return res.status(500).json({ error: 'Internal Server Error' });
+                }
+                return res.json({ status: `${updatedUsersData.length}` });
+            });
+        } else {
+            // If the user with the given id is not found, return a 404 response
+            return res.status(404).json({ error: 'User not found' });
+        }
+    });
+
+
+app.post('/api/users', (req, res) => {
+    const data = req.body;
+    usersData.push({ ...data, id: usersData.length + 1 });
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(usersData), (error, result) => {
+        if (error) {
+            return res.send(error);
+        } else {
+            return res.json({ status: "Success", id: usersData.length });
+        }
+    })
+
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
--- server.js
+++ server.js
@@ -1,108 +1,130 @@
 const express = require("express");
-const usersData = require("./MOCK_DATA.json");
-const fs = require("fs");
-const { error } = require("console");
+const mongoose = require("mongoose");
 
 // app is a handler function
 const app = express();
+app.use(express.json());
+
 const PORT = 9000;
 
-// middle ware example
-app.use((req, res, next)=>{
-    console.log("working");
-    return res.json({message: "Working"})
-    next();
-})
+// DB CONNECTION
+mongoose.connect('mongodb+srv://ankitkapoor8437:Zxcvbn_8437@crud.arhlgta.mongodb.net/practiceDatabase?retryWrites=true&w=majority')
+    .then(() => {
+        console.log("DB IS CONNECTED");
+    }).catch((error) => { console.log("Mongo Error", error) });
 
-
-app.use(express.json())
-// SSR
-app.get('/users', (req, res) => {
-    const html = ` <ul>
-            ${usersData.map((users) => `<li>${users.first_name}</li>`).join("")}
-        </ul>`
-    res.send(html);
+// SCHEMA
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    job_title: {
+        type: String,
+    },
+    gender: {
+        type: String,
+    }
 });
 
-// CSR
-app.get('/api/users/', (req, res) => {
-    return res.json(usersData);
+const User = mongoose.model("user", userSchema);
+
+
+
+
+// ROUTES
+
+// GET
+app.get('/api/users/', async (req, res) => {
+    try {
+        const allUsers = await User.find({});
+        return res.status(200).json(allUsers);
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
 });
 
-app.route("/api/users/:id").get((req, res) => {
-    const id = Number(req.params.id);
-    const user = usersData.find((users) => users.id === id);
-    console.log(user);
-    return res.json(user);
-})
-    .patch((req, res) => {
-        const id = Number(req.params.id);
-        const { first_name, last_name, email, gender, job_title } = req.body;
-        const dataToUpdate = usersData.find((users) => users.id === id);
-        console.log(dataToUpdate)
-        if (dataToUpdate) {
-            if (first_name) {
-                dataToUpdate.first_name =first_name;
-            }
-            
-            if (last_name) {
-                dataToUpdate.last_name =last_name;
-            }
-            
-            if (email) {
-                dataToUpdate.email =email;
-            }
-            
-            if (gender) {
-                dataToUpdate.gender =gender;
-            }
-            
-            if (job_title) {
-                dataToUpdate.job_title =job_title;
-            }
-        }
-          // Write the updated data to the JSON file
-          fs.writeFile('./MOCK_DATA.json', JSON.stringify(usersData), (error) => {
-            if (error) {
-                console.error(error);
-                return res.status(500).json({ error: 'Internal Server Error' });
-            }
-            return res.json(usersData);
+// POST
+app.post('/api/users', async (req, res) => {
+    const { first_name, last_name, email, job_title, gender } = req.body;;
+
+    if (!first_name || !last_name || !gender || !email || !job_title) {
+        return res.status(400).json({ message: "All fields are required!" });
+    }
+
+    try {
+        const createUser = await User.create({
+            first_name,
+            last_name,
+            email,
+            job_title,
+            gender
         });
-    })
-    .delete((req, res) => {
-        const id = Number(req.params.id);
-        const updatedUsersData = usersData.filter((user) => user.id !== id);
 
-        if (updatedUsersData.length < usersData.length) {
-            // Write the updated data to the JSON file
-            fs.writeFile('./MOCK_DATA.json', JSON.stringify(updatedUsersData), (error) => {
-                if (error) {
-                    console.error(error);
-                    return res.status(500).json({ error: 'Internal Server Error' });
-                }
-                return res.json({ status: `${updatedUsersData.length}` });
+        return res.status(201).json(createUser);
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+// GET BY ID, PUT, DELETE
+app.route("/api/users/:id").get(async (req, res) => {
+    try {
+        const userById = await User.findById(req.params.id);
+        if (!userById) {
+            return res.status(404).json({message:"User not found!"}); 
+        }
+        return res.status(200).json(userById);
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+})
+    .patch(async (req, res) => {
+        const id = req.params.id;
+        const { first_name, last_name, email, gender, job_title } = req.body;
+
+        try {
+            // Find the user by id and update the fields
+            const dataToUpdate = req.body;
+            await User.findByIdAndUpdate(id, {
+                first_name,
+                last_name,
+                gender,
+                job_title
             });
-        } else {
-            // If the user with the given id is not found, return a 404 response
-            return res.status(404).json({ error: 'User not found' });
+
+            if (!dataToUpdate) {
+                return res.status(404).json({ message: "User not found" });
+            }
+            console.log(dataToUpdate);
+            return res.status(200).json(dataToUpdate);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: "Internal Server Error" });
+        }
+    })
+    .delete(async (req, res) => {
+        const id = req.params.id;
+        try {
+            const deletedUser = await User.findByIdAndDelete(id);
+            if (!deletedUser) {
+                return res.status(404).json({message:"User not found!"}); 
+            }
+            return res.status(200).json({ message: `Successfully deleted user with id: ${id}` });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Internal Server Error' });
         }
     });
-
-
-app.post('/api/users', (req, res) => {
-    const data = req.body;
-    usersData.push({ ...data, id: usersData.length + 1 });
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(usersData), (error, result) => {
-        if (error) {
-            return res.send(error);
-        } else {
-            return res.json({ status: "Success", id: usersData.length });
-        }
-    })
-
-});
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
